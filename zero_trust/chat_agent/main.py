174a3deaa2f2c7a28d52e3843bf511c6d9c44695
David Wood <david.p.wood@gmail.com>
2026-03-24 08:59:46 +0000

    use consistent logging.INFO

--- zero_trust/chat_agent/main.py
+++ zero_trust/chat_agent/main.py
@@ -9,7 +9,7 @@ from litellm import Choices, ModelResponse
 dotenv.load_dotenv()
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
 )
 logger = logging.getLogger("chat_agent")
